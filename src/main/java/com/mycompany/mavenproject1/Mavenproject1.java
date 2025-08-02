cddea4890c1e769a46996018fa1271f2595941e5
alumno <alumno@10.0.0.22>
2025-08-02 17:16:47 -0600

    "Cambios 02/08/2025"

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        String nombre,edad;
+        Scanner lector=new Scanner(System.in);
+           System.out.println("Ingrese su nombre");
+           nombre=lector.next();
+        System.out.println("Ingrese su edad");
+        edad=lector.next();
+    }
+}
